159ecd8e73c2b0075bd41e4516927b4d1257b651
davidbuyck <47149695+davidbuyck@users.noreply.github.com>
2026-01-01 15:20:13 -0600

    Added first new prototype

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-if (toggle) {
-  toggle.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-  });
-}
